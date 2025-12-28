5a48826424e1e73d7e7977bde103bb3e00606a0b
thomastepi <tepexminem2013@gmail.com>
2025-12-28 14:03:57 -0500

    fix useSearchParams suspense error in contact us page

--- src/app/contact-us/page.jsx
+++ src/app/contact-us/page.jsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import ContactUsPage from "@/scenes/contact";
 
 export const metadata = {
@@ -7,9 +8,9 @@ export const metadata = {
 
 const ContactUs = () => {
   return (
-    <>
+    <Suspense fallback={<div>Loading…</div>}>
       <ContactUsPage />
-    </>
+    </Suspense>
   );
 };
 
